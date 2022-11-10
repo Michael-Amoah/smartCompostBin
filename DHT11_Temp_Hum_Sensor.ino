09818ed58fde02fb7f5253b4bd2069d5b16216e6
Michael Amoah <64275551+Michael-Amoah@users.noreply.github.com>
2022-11-10 02:07:55 -0500

    Add files via upload
    New DHT11_Temp_Hum_Sensor.ino file

--- DHT11_Temp_Hum_Sensor.ino
+++ DHT11_Temp_Hum_Sensor.ino
@@ -5,56 +5,35 @@
  * 
  */
 
-#include "DHT.h"
+#include <DHT.h>         
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11
+#define DHTPIN 2          // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11     
 
-DHT dht(DHTPIN, DHTTYPE);
-void setup() {
+DHT dht(DHTPIN, DHTTYPE);   
+
+void setup () {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println(F("DHT11 Test"));
-  
   dht.begin();
+  Serial.begin(9600);         
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // Wait a few seconds between measurements
-  delay(2000);
+void loop (){
+    // put your main code here, to run repeatedly:
+    float H = dht.readHumidity();    
+    // Read temperature as Celsius
+    float T = dht.readTemperature();    
+    // Read temperature as Fahrenheit
+    // float f = dht.readTemperature(true);
 
-    float h = dht.readHumidity();
-  // Read temperature as Celsius
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
-
-  // Check if any readings failed
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-  
-  Serial.print(F(" Humidity: "));
-  Serial.print(h);
-  Serial.print("%\t");
-  Serial.print(F("Temperature: "));
-  Serial.print(t);
-  Serial.print(F("C"));
-  Serial.print("\t");
-  Serial.print(f);
-  Serial.println(F("F"));
-
-  if (t <= 40) {
-    Serial.println("Temp too cold! \nTurn up heat");
-  }
-
-  if (40 < t && t < 65) {
-    Serial.println("Perfect temperature!");
-  }
-  
-  if (t >= 65) {
-    Serial.println("Temp too hot! \nTurn down heat");
-  }
+    // Check if any reads failed and if exit
+    if (isnan(H) || isnan(T)){
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+ 
+    // Combine Humidity and Temperature into single string
+    String dhtData = String(H) + "," + String(T);
+    Serial.println(dhtData);
+    delay(2000);   // Wait two seconds between measurements
 }
